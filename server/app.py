ce20ed916e70dd44d190715092d8e58cf8b48041
willymusyoka <willykyeni@gmail.com>
2025-09-21 18:03:45 +0300

    Implement CRUD operations for messages and add migration files for Message model

--- server/app.py
+++ server/app.py
@@ -16,11 +16,37 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    body = data.get('body')
+    username = data.get('username')
+    if not body or not username:
+        return make_response({'error': 'Missing body or username'}, 400)
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    body = data.get('body')
+    if body:
+        message.body = body
+        db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return make_response('', 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
